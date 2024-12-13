7ea8813e800ed3e3feab8fbac58f7dbd0d5d76f1
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2024-12-13 12:57:13 +0200

    Refactor list item and change style

--- src/components/ProfileListItem.tsx
+++ src/components/ProfileListItem.tsx
@@ -18,30 +18,32 @@ const typeBadgeNames: Record<ProfileType, string> = {
   sessionBox: 'SessionBox One',
 };
 
-export default function ProfileListItem(props: ProfileListItemProps) {
-  const { profile, onLaunch } = props;
-
+export default function ProfileListItem({
+  profile,
+  onLaunch,
+  onDelete,
+}: ProfileListItemProps) {
   return (
-    <li className="flex flex-row gap-6 justify-between items-center">
+    <li className="flex flex-col sm:flex-row gap-6 justify-between sm:items-center border-b-2 border-slate-200 pb-2">
       <div className="flex flex-row gap-6 justify-start items-center">
-        <p className="">{profile.name}</p>
+        <p>{profile.name}</p>
         <div
           className={`${typeBadgeBackgrounds[profile.type]} px-3 py-1 rounded-full`}
         >
           <p className="text-xs">{typeBadgeNames[profile.type]}</p>
         </div>
       </div>
-      <div className="flex flex-row gap-2 justify-end items-center">
+      <div className="flex flex-row gap-2 justify-between sm:justify-end items-center">
         <button
           type="button"
-          className="bg-yellow-400 px-3 py-1 rounded-md hover:bg-yellow-300 text-yellow-950"
+          className="bg-yellow-400 px-3 py-1 rounded-md hover:bg-yellow-300 text-yellow-950 grow sm:flex-none"
           onClick={onLaunch}
         >
           Launch
         </button>
         <button
           type="button"
-          className="bg-slate-300 hover:bg-red-400 px-3 py-1 rounded-md text-slate-900 ease-linear duration-100"
+          className="bg-slate-300 hover:bg-red-400 px-3 py-1 rounded-md text-slate-900 ease-linear duration-100 grow sm:flex-none"
           onClick={onDelete}
         >
           Delete
